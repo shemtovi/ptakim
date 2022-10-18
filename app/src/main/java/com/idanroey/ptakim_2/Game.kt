bc1d44a63542cefa01dafa55e47f5ae184a26bd8
Fabian665 <fabian665@gmail.com>
2022-10-19 01:35:05 +0300

    merged feedback branch to newVersion

--- app/src/main/java/com/idanroey/ptakim_2/Game.kt
+++ app/src/main/java/com/idanroey/ptakim_2/Game.kt
@@ -1,25 +1,35 @@
 package com.idanroey.ptakim_2
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.os.CountDownTimer
+import android.content.Context
+import android.graphics.Color
+import android.graphics.Typeface
+import android.os.*
+import android.view.HapticFeedbackConstants
 import android.view.View
 import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
 
 class Game : AppCompatActivity() {
 
+    // Game elements
     private var roundNumber = 1
     private lateinit var game: Ptakim
     private lateinit var team1: Team
     private lateinit var team2: Team
+
+    // Text views
     private lateinit var player1Score: TextView
     private lateinit var player2Score: TextView
+    private lateinit var team1View: TextView
+    private lateinit var team2View: TextView
     private lateinit var word: TextView
-    private  lateinit var leftNumOfWordsTextView: TextView
+    private lateinit var leftNumOfWordsTextView: TextView
 
 
     //timer var
@@ -29,17 +39,28 @@ class Game : AppCompatActivity() {
     private lateinit var timerView: TextView
     private lateinit var timer: CountDownTimer
 
+    // Resources
+    private lateinit var vibrator: Vibrator
     private lateinit var bilder: AlertDialog.Builder
     private  lateinit var dialog: AlertDialog
 
 
     @SuppressLint("MissingInflatedId")
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
 
         val numberOfWords = this.intent.getIntExtra("numberOfWords", 20)
         val selectedCategories = this.intent.getIntArrayExtra("filteredCategories")!!
+
+        vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val vibratorManager= getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
+            vibratorManager.defaultVibrator
+
+        } else {
+            getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+        }
 
         timerView = findViewById(R.id.timer)
         leftNumOfWordsTextView = findViewById(R.id.leftWordsTextViewInt)
@@ -52,15 +73,18 @@ class Game : AppCompatActivity() {
         player1Score = findViewById(R.id.player1_ScoreBoard)
         player2Score = findViewById(R.id.player2_ScoreBoard)
         word = findViewById(R.id.word)
+        team1View = findViewById(R.id.player_1)
+        team2View = findViewById(R.id.player_2)
 
         game.startRound()
+        updateTeamsViews()
         word.text = game.drawPetek()
 
         createRoundAlertDialog(roundNumber)
 
-
         findViewById<ImageButton>(R.id.right_button).setOnClickListener {
             if(mTimerRunning){
+                it.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
                 game.rightGuess()
                 if (game.currentTeam === team1) {
                     player1Score.text = team1.getScore().toString()
@@ -78,7 +102,13 @@ class Game : AppCompatActivity() {
 
         findViewById<ImageButton>(R.id.wrong_button).setOnClickListener {
             if(mTimerRunning){
-                game.wrongGuess()
+            val handler = Handler(Looper.getMainLooper())
+            it.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)
+            handler.postDelayed(
+                {it.performHapticFeedback(HapticFeedbackConstants.VIRTUAL_KEY)},
+                100
+            )
+            game.wrongGuess()
                 if (game.currentTeam === team1) {
                     player1Score.text = team1.getScore().toString()
                 } else {
@@ -92,23 +122,32 @@ class Game : AppCompatActivity() {
             if(mTimerRunning) pauseTimer()
             createStopAlertDialog()
         }
-
-
     }
 
 
+    @RequiresApi(Build.VERSION_CODES.Q)
     private fun nextRound() {
         if (roundNumber < 3) {
             roundNumber++
+            vibrator.vibrate(VibrationEffect.createPredefined(VibrationEffect.EFFECT_HEAVY_CLICK))
             resetTimer()
             game.teamSwitch()
+            updateTeamsViews()
             game.startRound()
             word.text = game.drawPetek()
-
         }
         //popup window
         createRoundAlertDialog(roundNumber)
     }
+
+    fun updateTeamsViews() {
+        val textColor: String = "#" + Integer.toHexString(ResourcesCompat.getColor(resources, R.color.text_color, null))
+        team1View.typeface = if (game.currentTeam === team1) Typeface.DEFAULT_BOLD else Typeface.DEFAULT
+        team1View.setTextColor(Color.parseColor(if (game.currentTeam === team1) "#5adbb5" else textColor))
+        team2View.typeface = if (game.currentTeam === team2) Typeface.DEFAULT_BOLD else Typeface.DEFAULT
+        team2View.setTextColor(Color.parseColor(if (game.currentTeam === team2) "#5adbb5" else textColor))
+    }
+
     //timer functions
     fun startTimer() {
         timer = object : CountDownTimer(mTimeLeftInMillis, 100) {
@@ -122,25 +161,23 @@ class Game : AppCompatActivity() {
             override fun onFinish() {
                 resetTimer()
                 game.teamSwitch()
+                updateTeamsViews()
                 //popup window
                 createStopAlertDialog()
             }
         }.start()
         mTimerRunning = true
-
     }
 
     fun resetTimer(){
         pauseTimer()
         mTimeLeftInMillis = START_TIME_IN_MILLIS
         timerView.text = (mTimeLeftInMillis / 1000).toString()
-
     }
 
-    fun  pauseTimer(){
+    fun pauseTimer(){
         timer.cancel()
         mTimerRunning = false
-
     }
 
     //dialog functions
@@ -165,9 +202,9 @@ class Game : AppCompatActivity() {
 
     fun createRoundAlertDialog(round:Int){
         createNewAlertDialog()
-        var upperText: TextView? = dialog.findViewById<TextView>(R.id.upperText)
-        var lowerText: TextView? = dialog.findViewById<TextView>(R.id.lowerText)
-        var  dialogButton: Button? = dialog.findViewById(R.id.alertDialogButton)
+        val upperText = dialog.findViewById<TextView>(R.id.upperText)
+        var lowerText = dialog.findViewById<TextView>(R.id.lowerText)
+        val dialogButton: Button? = dialog.findViewById(R.id.alertDialogButton)
         if(round == 1){
          //round 1 text
             if (upperText != null) {
